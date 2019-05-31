a097b16410586ada0003c68e088047812cdf3925
Jacob <jakeguss@gmail.com>
2019-05-31 13:31:50 -0600

    going gold

---
+++ build/precache-manifest.c73a04d62cd482d2ad69bfd6fbcf71e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677e051c83c6f709f23758b65c7b3055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0672a320d0904a6917b",
+    "url": "/static/css/main.ca90d84e.chunk.css"
+  },
+  {
+    "revision": "0c612d9bf54d209e4554",
+    "url": "/static/js/2.2a05fb22.chunk.js"
+  },
+  {
+    "revision": "d0672a320d0904a6917b",
+    "url": "/static/js/main.89594b25.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import List from './List';
-import ListForm from './Form';
-import Filter from './Filter';
+import React from 'react'
+import List from './List'
+import ListForm from './Form'
+import Filter from './Filter'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -9,9 +13,16 @@ import Col from 'react-bootstrap/Col'
 
 import './App.css';
 
+library.add(faArrowLeft)
+
 function App() {
   return (
       <div className="App">
+        <div className="portfolioBar">
+          <div className="arrowContainer">
+            <a href="https://jake-guss.herokuapp.com/portfolio"><FontAwesomeIcon className="arrow-icon" icon="arrow-left" color="white" size="3x" /></a>
+          </div>
+        </div>
         <Container>
           <Row>
             <Col xs={12}><h1 className="title">The Greatest Redux List Ever</h1></Col>
